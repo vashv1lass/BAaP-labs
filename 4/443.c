f3ceacc4982567df5ab48339d1b6eef650f84321
max-vashkevich <maxim.vashkevich2007@gmail.com>
2024-10-03 14:31:40 +0300

    better lab 4

--- 4/443.c
+++ 4/443.c
@@ -1,9 +1,6 @@
 /*
     Лабораторная работа 4. Обработка одномерных массивов.
     Вариант 5, задание 4.4.3
-
-    Реализация "в лоб" O(n^2). Для ускорения программы до O(n*log(n)) можно
-    решить задачу при помощи быстрой сортировки или сортировки слиянием.
 */
 
 #include <stdio.h>
@@ -11,86 +8,83 @@
 #include <locale.h>
 #include <stdbool.h>
 
-void terminate(const char* message)
-{
-    puts(message);
-    exit(EXIT_SUCCESS);
-}
-
-bool found(int* x, int size, int element)
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (x[i] == element)
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
 typedef struct
 {
-    int* data;
-    int size;
+	int* data;
+	int size;
 } DynamicArray;
 
-
-DynamicArray removeDuplicate(DynamicArray x)
+void terminate(const char* message)
 {
-    DynamicArray unique;
-    unique.size = 1;
-    unique.data = (int*)malloc(unique.size * sizeof(int));
-    unique.data[0] = x.data[0];
+	puts(message);
+	exit(EXIT_SUCCESS);
+}
 
-    for (int i = 1; i < x.size; i++)
-    {
-        if (!found(unique.data, unique.size, x.data[i]))
-        {
-            unique.data = (int*)realloc(unique.data, ++unique.size * sizeof(int));
-            unique.data[unique.size - 1] = x.data[i];
-        }
-    }
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
-    return unique;
+bool found(int* x, size_t size, int element)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		if (x[i] == element)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void removeDuplicate(DynamicArray* x) {
+	int newSize = x->size;
+	
+	for (size_t i = 0; i < newSize; i++) {
+		if (found(x->data, i, x->data[i])) {
+			swap(&x->data[i], &x->data[newSize - 1]);
+			newSize--;
+			i--;
+		}
+	}
+	
+	x->data = realloc(x->data, newSize * sizeof(int));
+	x->size = newSize;
 }
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
+	setlocale(LC_ALL, "rus");
 
-    DynamicArray x;
-    puts("Введите размер массива x: ");
-    if (!scanf("%d", &x.size))
-    {
-        terminate("Проверьте корректность введённых данных!");
-    }
+	DynamicArray x;
+	puts("Введите размер массива x: ");
+	if (!scanf("%d", &x.size))
+	{
+		terminate("Проверьте корректность введённых данных!");
+	}
 
-    x.data = (int*)malloc(x.size * sizeof(int));
-    puts("Введите элементы массива: ");
-    for (int i = 0; i < x.size; i++)
-    {
-        if (!scanf("%d", &x.data[i]))
-        {
-            x.data = NULL;
-            free(x.data);
+	x.data = (int*)malloc(x.size * sizeof(int));
+	puts("Введите элементы массива: ");
+	for (size_t i = 0; i < x.size; i++) {
+		if (!scanf("%d", &x.data[i])) {
+			free(x.data);
+			x.data = NULL;
 
-            terminate("Проверьте корректность введённых данных!");
-        }
-    }
+			terminate("Проверьте корректность введённых данных!");
+		}
+	}
+	
+	removeDuplicate(&x);
+	
+	puts("Массив после удаления всех повторяющихся элементов: ");
+	for (size_t i = 0; i < x.size; i++)
+	{
+		printf("%d ", x.data[i]);
+	}
+	
+	free(x.data);
+	x.data = NULL;
 
-    DynamicArray unique = removeDuplicate(x);
-    puts("Массив после удаления всех повторяющихся элементов: ");
-    for (int i = 0; i < unique.size; i++)
-    {
-        printf("%d ", unique.data[i]);
-    }
-    puts("");
-
-    x.data = NULL;
-    free(x.data);
-    unique.data = NULL;
-    free(unique.data);
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
